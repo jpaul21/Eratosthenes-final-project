1d3b5db9ff9da89e6bead0cab421c86434e1bb06
Joshua <jhwang@stevens.edu>
2020-08-19 14:41:12 -0400

    cpp edit

--- EratosthenesSieve.cpp
+++ EratosthenesSieve.cpp
@@ -2,24 +2,25 @@
 
 #include <cstdint>
 #include <iostream>
+#include <cstdint>
 
 using namespace std;
 
-uint32_t eratosthenes(uint32_t maxint, bool* isNotPrime) {
-  memset(isNotPrime, 0, maxint + 1);  // set the entire array to 0s
-  // 0 = prime, 1 = not prime
-  uint32_t numPrimes = 1;  // special case of 2 is prime
-  for (uint32_t i = 3; i < maxint;
-       i += 2) {  // no need to handle even numbers, so do every other number
-    if (!isNotPrime[i]) {  // if the number is prime, cancel out its multiples
-      for (uint32_t j = i * i; j < maxint; j += i * 2)
-        isNotPrime[j] = true;  // start at i*i because that should be the
-                               // smallest multiple we haven't handled
-      numPrimes++;  // if when we get to a number, it is not marked, it is
-                    // prime.
-    }
-  }
-  return numPrimes;
+uint32_t eratosthenes(uint32_t maxint, uint32_t* isNotPrime) {
+  // memset(isNotPrime, 0, maxint + 1);  // set the entire array to 0s
+  // // 0 = prime, 1 = not prime
+  // uint32_t numPrimes = 1;  // special case of 2 is prime
+  // for (uint32_t i = 3; i < maxint;
+  //      i += 2) {  // no need to handle even numbers, so do every other number
+  //   if (!isNotPrime[i]) {  // if the number is prime, cancel out its multiples
+  //     for (uint32_t j = i * i; j < maxint; j += i * 2)
+  //       isNotPrime[j] = true;  // start at i*i because that should be the
+  //                              // smallest multiple we haven't handled
+  //     numPrimes++;  // if when we get to a number, it is not marked, it is
+  //                   // prime.
+  //   }
+  // }
+  // return numPrimes;
 }
 
 int main() {
@@ -28,7 +29,7 @@ int main() {
   uint32_t input;
   cin >> input;
   cout << endl;
-  bool* isNotPrime = new bool[input];
+  uint32_t* isNotPrime = new uint32_t[input];
   cout << "Primes less than " << input << ": "
        << eratosthenes(input - 1, isNotPrime) << endl;
   delete[] isNotPrime;
