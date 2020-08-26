5858261be0c1e464eba18e8e923cd29d2d78d870
Josh <jhwang@stevens.edu>
2020-08-26 19:23:48 -0400

    works only for <65

--- funcs2.s
+++ funcs2.s
@@ -1,6 +1,7 @@
 .global _Z12eratosthenesjPj	@r3=bit#; r4=32*xbits; r5=# of primes; r6=
 _Z12eratosthenesjPj:		@r7=overflow bit; r8=next odd#; r9=element to load/store
-  lsr   r2, r0, #5		@r10=normal op; r11=remaining shifts in element; r12=shifts needed
+  push  {r4-r12}		@r10=normal op; r11=remaining shifts in element; r12=shifts needed
+  lsr   r2, r0, #5
   add   r2, #1
   mov   r3, #0
  set:
@@ -8,13 +9,15 @@ _Z12eratosthenesjPj:		@r7=overflow bit; r8=next odd#; r9=element to load/store
   subs  r2, #1
   bne   set
   mov   r3, #1
-  mov   r5, r3
+  mov   r5, #0
   mov   r4, #32
   ldr   r8, =0x80000000
+  mov   r2, r8
   mov   r12, #3
   mov   r9, #0
  1:
-  add   r5, #1			@# of primes =+1
+  add   r5, #1			@prime num +=1
+  mov   r8, r2
   ldr   r10, [r1, r9]
   mov   r7, r8			@reset overflow bit
  2:
@@ -35,39 +38,48 @@ _Z12eratosthenesjPj:		@r7=overflow bit; r8=next odd#; r9=element to load/store
   orr   r7, r10
   str   r7, [r1, r9]
   sub   r9, #4
+  ldr   r10, [r1, r9]
  4:
+  cmp   r12, r0
+  bge   store
   add   r3, #1			@next +1 value
   add   r12, #2			@increment +=2 (shifter)
-  lsrs  r8, #1
+  lsr   r2, #1
+  tst   r10, r2
+  bne   4b
+  cmp   r2, #0
   bne   1b
   add   r4, #32
-  ldr   r8, =0x800000000
-  lsr   r9, r4, #3
-  sub   r9, #4
+  ldr   r2, =0x800000000
+  add   r9, #4
+ @ lsr   r9, r4, #3
+ @ sub   r9, #4
   ldr   r10, [r1, r9]
  subr:
-  ands  r2, r10, r8
+  tst   r10, r2
   beq   1b
-  ror   r8, #1
+  ror   r2, #1
   bl    subr
  reset:
   add   r9, #4
-  ldr   r8, =0x80000000
+  ldr   r2, =0x80000000
  5:				@final countdown
   str   r10, [r1, r9]
-  ldr   r10, [r1, r9]
  final:
+  ldr   r10, [r1, r9]
   add   r3, #1
-  lsrs  r8, #1
+  lsrs  r2, #1
   beq   reset
-  ands  r10, r8
+  tst   r10, r2
   bne   final
   add   r5, #1
-  orr   r10, r8
+  orr   r10, r2
   str   r10, [r1, r9]
   cmp   r3, r0
-  blt   reset
+  blt   final
+ store:
   mov   r0, r5
+  pop   {r4-r12}
   bx    lr
  6:
   add   r9, #4
